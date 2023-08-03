3f788d7ccfa94b86c63896a7cb5cf3599367b9ef
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2023-08-03 11:10:44 +0900

    testとimplementationを追加した。

--- src/use_cases/pdf_to_markdown_converter.py
+++ src/use_cases/pdf_to_markdown_converter.py
@@ -2,11 +2,17 @@ from entities.markdown_content_base import MarkdownContentBase, MarkdownTagName
 from entities.pdf_content_base import PDFContentBase
 
 
+class PDFFontsToTagConfig:
+    EQUATION_FONTS = ["YNTGTF+CMR10"]
+    HEADERS_FONTS = ["ZEAUGF+NimbusRomNo9L-Medi"]
+    PLAIN_TEXT_FONTS = []
+
+
 class PDF2MarkdownConverter:
     TAG_BY_LOWEST_FONTSIZE = {
-        15: MarkdownTagName.HEADER_1,
-        12: MarkdownTagName.HEADER_2,
-        11: MarkdownTagName.HEADER_3,
+        MarkdownTagName.HEADER_1: 15,
+        MarkdownTagName.HEADER_2: 12,
+        MarkdownTagName.HEADER_3: 11,
     }
 
     def __init__(self) -> None:
@@ -16,4 +22,31 @@ class PDF2MarkdownConverter:
         self,
         pdf_contents: list[PDFContentBase],
     ) -> list[MarkdownContentBase]:
-        return [pdf_content.raw_text for pdf_content in pdf_contents]
+        md_contents = []
+        previous_fontsize = None
+        previous_fontname = None
+        for pdf_content in pdf_contents:
+            if self._is_headers_tag(pdf_content):
+                md_contents.append(MarkdownContentBase(self._clasify_header_type(pdf_content), pdf_content.raw_text))
+
+        return md_contents
+
+    def _is_headers_tag(self, pdf_content: PDFContentBase) -> bool:
+        """headers tagか否かを判定してboolを返す。"""
+        if pdf_content.fontname_mode not in PDFFontsToTagConfig.HEADERS_FONTS:
+            return False
+        if pdf_content.fontsize_mode < self.TAG_BY_LOWEST_FONTSIZE[MarkdownTagName.HEADER_3]:
+            return False
+        return True
+
+    def _clasify_header_type(self, pdf_content: PDFContentBase) -> MarkdownTagName:
+        """どのheaderかを判定して返す"""
+        fontsize = pdf_content.fontsize_mode
+        fontname = pdf_content.fontname_mode
+        if fontsize >= self.TAG_BY_LOWEST_FONTSIZE[MarkdownTagName.HEADER_1]:
+            return MarkdownTagName.HEADER_1
+        elif fontsize >= self.TAG_BY_LOWEST_FONTSIZE[MarkdownTagName.HEADER_2]:
+            return MarkdownTagName.HEADER_2
+        elif fontsize >= self.TAG_BY_LOWEST_FONTSIZE[MarkdownTagName.HEADER_3]:
+            return MarkdownTagName.HEADER_3
+        return MarkdownTagName.HEADER_4
--- test/pdf_to_md_converter/test_pdf_to_markdown_converter.py
+++ test/pdf_to_md_converter/test_pdf_to_markdown_converter.py
@@ -1,20 +1,20 @@
 import pytest
 
-from entities.markdown_content_base import MarkdownContentBase
+from entities.markdown_content_base import MarkdownContentBase, MarkdownTagName
 from entities.pdf_content_base import PDFContentBase, PDFContentTypes
-from use_cases.pdf_to_markdown_converter import PDF2MarkdownConverter
+from use_cases.pdf_to_markdown_converter import PDF2MarkdownConverter, PDFFontsToTagConfig
 import pdfminer
 
 
-def test_pdf_content_is_converted_to_markdown_content() -> None:
+def test_continuous_pdf_contents_without_headers_font_are_joined_and_converted_to_plaintext_md_content() -> None:
     # Arrange
     pdf_contents = [
         PDFContentBase(
-            bbox=None,
+            bbox=(),
             raw_text="Off-Policy Evaluation for Large Action Spaces via Embeddings\n",
             content_type=PDFContentTypes.TEXT_LINE_HORIZONTAL,
-            fontsize_mode=18.0,
-            fontname_mode="ZEAUGF+NimbusRomNo9L-Medi",
+            fontsize_mode=18,
+            fontname_mode=PDFFontsToTagConfig.HEADERS_FONTS[0],
         ),
     ]
     sut = PDF2MarkdownConverter()
@@ -23,5 +23,36 @@ def test_pdf_content_is_converted_to_markdown_content() -> None:
     md_contents = sut.convert(pdf_contents)
 
     # Assert
-    md_contents_expected = [MarkdownContentBase()]
+    md_contents_expected = [
+        MarkdownContentBase(
+            MarkdownTagName.HEADER_1,
+            "Off-Policy Evaluation for Large Action Spaces via Embeddings\n",
+        )
+    ]
+    assert md_contents == md_contents_expected
+
+
+def test_continuous_pdf_contents_without_headers_font_are_joined_and_converted_to_plaintext_md_content() -> None:
+    # Arrange
+    pdf_contents = [
+        PDFContentBase(
+            bbox=(),
+            raw_text="Off-Policy Evaluation for Large Action Spaces via Embeddings\n",
+            content_type=PDFContentTypes.TEXT_LINE_HORIZONTAL,
+            fontsize_mode=18,
+            fontname_mode=PDFFontsToTagConfig.HEADERS_FONTS[0],
+        ),
+    ]
+    sut = PDF2MarkdownConverter()
+
+    # Act
+    md_contents = sut.convert(pdf_contents)
+
+    # Assert
+    md_contents_expected = [
+        MarkdownContentBase(
+            MarkdownTagName.HEADER_1,
+            "Off-Policy Evaluation for Large Action Spaces via Embeddings\n",
+        )
+    ]
     assert md_contents == md_contents_expected
